c3e04b8394ceaa0554b35fb43f6d9ff25beead09
Sharif Olorin <sio@tesser.org>
2014-05-21 17:50:26 +1000

    Do some more parsing

--- src/Perfdata.hs
+++ src/Perfdata.hs
@@ -8,6 +8,8 @@ import Control.Applicative
 import Data.Attoparsec.ByteString.Char8
 import qualified Data.ByteString as S
 import Data.Word
+import Data.List hiding (takeWhile)
+import qualified Data.Map as M
 
 data Item = Item {
     name :: S.ByteString,
@@ -33,3 +35,34 @@ item = Item `fmap` ident <* separator <*> val
 line :: Parser [Item]
 line = many item
 
+type ItemMap = M.Map S.ByteString S.ByteString
+
+mapItems :: [Item] -> ItemMap
+mapItems = foldl (\m i -> M.insert (name i) (value i) m) M.empty
+
+data ServicePerfdata = ServicePerfData {
+    serviceDescription :: S.ByteString,
+    serviceState       :: S.ByteString,
+    serviceStateType       :: S.ByteString
+}
+
+data HostOrService = Service ServicePerfdata | Host
+
+data Perfdatum = Perfdatum {
+    dataType :: HostOrService,
+    timestamp :: Word64,
+    hostname :: S.ByteString,
+    hostState :: S.ByteString,
+    hostStateType :: S.ByteString,
+    perfString    :: S.ByteString
+}
+
+type ParserError = [Char]
+
+parseLine :: S.ByteString -> Result [Item]
+parseLine = parse line
+
+extractItems :: Result [Item] -> Either ParserError ItemMap
+extractItems (Done _ is) = Right $ mapItems is
+extractItems (Fail _ ctxs err) = Left $ concat $ err : "\n" : (intercalate "," ctxs) : []
+extractItems (Partial f) = extractItems (f "")
