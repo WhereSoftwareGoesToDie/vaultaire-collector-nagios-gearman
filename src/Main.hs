59aa0ad062a71c5495bbfe320b8cc2ef49e91775
Sharif Olorin <sio@tesser.org>
2014-05-22 16:58:41 +1000

    Add some CLI options

--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,47 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Main where
 
 import System.Gearman.Worker
 import System.Gearman.Connection
 import Control.Monad
+import Options.Applicative
 
-main :: IO ()
-main = do
+data CollectorOptions = CollectorOptions {
+    optGearmanHost :: String,
+    optGearmanPort :: String
+}
+
+opts :: Parser CollectorOptions
+opts = CollectorOptions
+       <$> strOption
+           (long "gearman-host"
+            <> short 'h'
+            <> value "localhost"
+            <> metavar "GEARMANHOST"
+            <> help "Hostname of Gearman server.")
+       <*> strOption
+           (long "gearman-port"
+            <> short 'p'
+            <> value "4370"
+            <> metavar "GEARMANPORT"
+            <> help "Port number Gearman server is listening on.")
+
+collectorOptionParser :: ParserInfo CollectorOptions
+collectorOptionParser = 
+    info (helper <*> opts)
+    (fullDesc <>
+        progDesc "Vaultaire collector for Nagios with mod_gearman" <>
+        header "vaultaire-collector-nagios-gearman - daemon to write Nagios perfdata to Vaultaire")
+
+collector :: CollectorOptions -> IO ()
+collector CollectorOptions{..} = do
     let processDatum _ = return $ Right "test"
-    runGearman "localhost" "4370" $ runWorker 2 $ do
+    runGearman optGearmanHost optGearmanPort $ runWorker 2 $ do
         void $ addFunc "service" processDatum Nothing
         work
     putStrLn "done"
+
+main :: IO ()
+main = execParser collectorOptionParser >>= collector
